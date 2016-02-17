5e40b64cda1bc0974251d8cb6df252c02894afdf
Nightflyyer <nkploveapple@gmail.com>
2016-02-16 17:06:24 -0800

    wrong packet id woops

--- app/Libraries/Packet.php
+++ app/Libraries/Packet.php
@@ -83,7 +83,7 @@ class Packet {
             //int32
             case 5:		//user id
             case 12:    //despawn user panel
-            case 16:   //spectator replay data?
+            case 15:   //spectator replay data?
             case 71:	//user rank
             case 75: 	//cho protocol
             case 92:	//ban status
